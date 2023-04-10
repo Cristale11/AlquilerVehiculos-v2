40152114336b855e1ccf2f3b62b3eab564bb5eee
cristian <cristiangalvezcaravaca@gmail.com>
2023-04-10 13:58:43 +0200

    Pongo mi nombre en el readme

--- src/main/java/org/iesalandalus/programacion/alquilervehiculos/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/alquilervehiculos/MainApp.java
@@ -4,16 +4,18 @@ import org.iesalandalus.programacion.alquilervehiculos.controlador.Controlador;
 import org.iesalandalus.programacion.alquilervehiculos.modelo.FactoriaFuenteDatos;
 import org.iesalandalus.programacion.alquilervehiculos.modelo.Modelo;
 import org.iesalandalus.programacion.alquilervehiculos.modelo.ModeloCascada;
-import org.iesalandalus.programacion.alquilervehiculos.vista.FactoriaVista;
+import org.iesalandalus.programacion.alquilervehiculos.vista.FactorioVista;
 import org.iesalandalus.programacion.alquilervehiculos.vista.Vista;
 
 public class MainApp {
 
 	public static void main(String[] args) {
-		Modelo modelo = new ModeloCascada(FactoriaFuenteDatos.FICHEROS);
-		Vista vista = FactoriaVista.TEXTO.crear();
+		Vista vista = FactorioVista.TEXTO.crear();
+
+		Modelo modelo = new ModeloCascada(FactoriaFuenteDatos.MEMORIA.crear());
+
 		Controlador controlador = new Controlador(modelo, vista);
+
 		controlador.comenzar();
 	}
-
 }
